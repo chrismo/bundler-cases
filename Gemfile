ce51655168c52f2748e18a8099dd7ca1e3c4c0e8
chrismo <chrismo@clabs.org>
2016-08-30 12:13:00 -0500

    Some Runner love.
    I realize I'm re-inventing my own test framework of sorts from scratch
    ... but ... I'm mostly doing it intentionally as a side-exercise.
    Some things you can just do yourself.
    
    That said, swapping in RSpec at some point may make sense and be simple
    enough to do.

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'https://rubygems.org' do
   gem 'builder'
   gem 'bundler'
   gem 'rake'
+  gem 'rainbow'
   gem 'rspec'
 end
--- run_cases.rb
+++ run_cases.rb
@@ -3,29 +3,71 @@
 require 'bundler/setup'
 
 require_relative 'lib/bundler_case'
+require 'rainbow'
 
-filter = ARGV[0]
+class BundlerCase
+  class Fail
+    def initialize(fn, title, message)
+      @fn = fn.sub(__dir__, '')
+      @title = title
+      @message = message
+    end
 
-def puts_title(title)
-  width = [title.length, 80].max
-  puts '=' * width
-  puts title.center(width)
-  puts '=' * width
-end
+    def to_s
+      Rainbow("FAILED: #{@title || @fn} - #{@message}").red
+    end
+  end
 
-def execute_case(fn)
-  c = eval(File.read(fn))
-  title = File.basename(fn, '.rb').gsub(/_/, ' ').split(/ /).map(&:capitalize).join(' ')
-  puts_title(title)
-  passed = c.test
-  puts "#{title}: #{passed ? 'Passed' : 'FAILED'}"
-  unless passed
-    puts c.failures
+  class Pass
+    def initialize(fn, title)
+      @fn = fn.sub(__dir__, '')
+      @title = title
+    end
+
+    def to_s
+      Rainbow("Passed: #{@title || @fn}").green
+    end
+  end
+
+  class Runner
+    def puts_title(title)
+      width = [title.length, 80].max
+      puts '=' * width
+      puts title.center(width)
+      puts '=' * width
+    end
+
+    def execute_case(fn)
+      c = eval(File.read(fn))
+      title = File.basename(fn, '.rb').gsub(/_/, ' ').split(/ /).map(&:capitalize).join(' ')
+      puts_title(title)
+      if c
+        passed = c.test
+        if passed then
+          Pass.new(fn, title)
+        else
+          puts c.failures.join("\n")
+          Fail.new(fn, title, c.failures.join("\n"))
+        end
+      else
+        Fail.new(fn, nil, 'No BundlerCase found')
+      end
+    end
+
+    def execute_cases(filter)
+      results = []
+      Dir[File.expand_path('cases/**/*.rb', __dir__)].each do |fn|
+        next if fn !~ /#{filter}/ if filter
+        begin
+          results << execute_case(fn)
+        rescue => e
+          puts "ERROR running #{fn}: #{[e.message, e.backtrace].join("\n")}"
+        end
+      end
+      pass, fail = results.partition { |r| r.is_a?(Pass) }
+      pass.concat(fail).each { |r| puts r.to_s }
+    end
   end
 end
 
-Dir[File.expand_path('cases/**/*.rb', __dir__)].each do |fn|
-  next if fn !~ /#{filter}/ if filter
-  execute_case(fn)
-end
-
+BundlerCase::Runner.new.execute_cases(filter = ARGV[0])
