5dd078eab92fba8c2bc54d1d77382fb36ab2c7f1
chrismo <chrismo@clabs.org>
2016-09-13 17:58:42 -0500

    Flesh out install cons update case

--- cases/conservative/install/after_gemfile_change_success.rb
+++ cases/conservative/install/after_gemfile_change_success.rb
@@ -4,15 +4,21 @@ BundlerCase.define do
       fake_gem 'foo', %w(1.0.0 1.0.1 1.1.0 2.0.0), [['bar', '~> 1.0']]
       fake_gem 'qux', %w(1.0.0 1.0.1 1.1.0 2.0.0), [['bar', '~> 1.0']]
       fake_gem 'bar', %w(1.0.0 1.0.1 1.1.0 2.0.0)
+
+      fake_gem 'tak', %w(1.0.0), [['hoq', '~> 1.0']]
+      fake_gem 'tak', %w(2.0.0), [['hoq', '~> 2.0']]
+      fake_gem 'hoq', %w(1.0.0 2.0.0)
     end
 
-    lock = ['foo 1.0.1', 'bar 1.0.0', 'qux 1.1.0']
+    lock = ['foo 1.0.1', 'bar 1.0.0', 'qux 1.1.0', 'tak 1.0.0', 'hoq 1.0.0']
 
     given_gemfile lock: lock do
       <<-G
       source 'fake' do
         gem "foo"
         gem "qux"
+
+        gem "tak"
       end
       G
     end
@@ -20,16 +26,21 @@ BundlerCase.define do
     expect_locked { lock }
   end
 
-  step "Update foo in Gemfile" do
+  step 'Change foo and tak in Gemfile then install' do
     given_gemfile do
       <<-G
       source 'fake' do
         gem "foo", "~> 2.0"
         gem "qux"
+
+        gem "tak", "~> 2.0"
       end
       G
     end
 
-    expect_locked { ['foo 2.0.0', 'bar 1.0.0', 'qux 1.1.0'] }
+    # bar isn't changed because it's a shared dependency, and this is how bundle install conservative updating works.
+    #
+    # hoq is changed because it's an isolated dependency.
+    expect_locked { ['foo 2.0.0', 'bar 1.0.0', 'qux 1.1.0', 'tak 2.0.0', 'hoq 2.0.0'] }
   end
 end
