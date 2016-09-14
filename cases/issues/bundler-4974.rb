7330d4968525905f40369525ede964fdc7278dc2
chrismo <chrismo@clabs.org>
2016-09-14 00:08:24 -0500

    New 4974 case

---
+++ cases/issues/bundler-4974.rb
@@ -0,0 +1,23 @@
+BundlerCase.define bundler_version: '1.12.5' do
+  step do
+    execute_bundler { 'gem install awesome_print' }
+  end
+
+  step do
+    given_gemfile lock: true do
+      <<-G
+source 'https://rubygems.org'
+
+gem "awesome_print"
+      G
+    end
+
+    execute_bundler { 'bundle install --deployment' }
+  end
+
+  step do
+    execute_bundler { 'bundle show --paths' }
+
+    expect_output { /\/out\/vendor\/bundle.*awesome_print/ }
+  end
+end
